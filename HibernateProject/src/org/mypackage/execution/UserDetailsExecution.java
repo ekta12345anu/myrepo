4145b1bf264c332e3a5131212adfb8a5f3818935
Ekta Sharma <ekta.narender.sharma@accenture.com>
2019-06-16 19:37:05 +0530

    hibernate config and record insertion

---
+++ HibernateProject/src/org/mypackage/execution/UserDetailsExecution.java
@@ -0,0 +1,25 @@
+package org.mypackage.execution;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+import org.mypackage.hibernate.UserDetails;
+
+public class UserDetailsExecution {
+
+	public static void main(String[] args) {
+
+		UserDetails userDetails = new UserDetails();
+		userDetails.setId(2);
+		userDetails.setUserName("Second User");
+		
+		SessionFactory sessiosFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+		Session session = sessiosFactory.openSession();
+		session.beginTransaction();
+		session.save(userDetails);
+		session.beginTransaction().commit();
+
+	}
+
+}
